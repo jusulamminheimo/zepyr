d8cff494b7a891b5a9dac8049797110e102f3c9c
Jussi-Tapio Lamminheimo <jusulamminheimo@gmail.com>
2021-04-20 00:05:44 +0300

    add build as a optional parameter

--- champion_scrape.py
+++ champion_scrape.py
@@ -33,16 +33,35 @@ def get_runes_by_champion_name(champion_name, is_aram):
 
     driver.switch_to.default_content()
 
-    items = driver.find_element_by_xpath(
-        '//*[@id="content"]/div/div[1]/div/div/div[5]/div/div[6]')
-    items.screenshot("items.png")
-
     runes = driver.find_element_by_xpath(
         '//*[@id="content"]/div/div[1]/div/div/div[5]/div/div[2]/div[1]')
     runes.screenshot("runes.png")
 
+    driver.quit()
+
+
+def get_build_by_champion_name(champion_name, is_aram):
+    driver = webdriver.Chrome(
+        options=options, executable_path=CHROMEDRIVER_PATH)
+    if(is_aram):
+        url = f"https://u.gg/lol/champions/aram/{champion_name}-aram"
+    else:
+        url = f"https://u.gg/lol/champions/{champion_name}/build"
+    driver.get(url)
+    WebDriverWait(driver, 20).until(
+        EC.frame_to_be_available_and_switch_to_it((By.ID, "sp_message_iframe_403856")))
+    button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+        (By.XPATH, '/html/body/div/div[2]/div[5]/button[2]')))
+    button.click()
+
+    driver.switch_to.default_content()
+
     abilities = driver.find_element_by_xpath(
         '//*[@id="content"]/div/div[1]/div/div/div[5]/div/div[3]/div[1]')
     abilities.screenshot("abilities.png")
+
+    items = driver.find_element_by_xpath(
+        '//*[@id="content"]/div/div[1]/div/div/div[5]/div/div[6]')
+    items.screenshot("items.png")
 
     driver.quit()
